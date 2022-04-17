45da20dae48b4dc2f71181fdb817d172d7f38424
jobcher <13028911306@163.com>
2022-04-17 21:22:58 +0800

    添加注释

--- restart-pod.sh
+++ restart-pod.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "删除重启pod"
 kubectl get po -n default | grep jobcher | awk '{print $1}' > restart_jobcher.sh
+# 每行头部插入
 sed -i 's/^/kubectl delete po &/g' restart_jobcher.sh
+# 每行尾部插入
 sed -i 's/$/& -n default/g' restart_jobcher.sh
 chmod +x restart_jobcher.sh
 ./restart_jobcher.sh
